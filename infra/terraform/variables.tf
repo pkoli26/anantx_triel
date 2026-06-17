cf504964447615d1efca508e25776e3fece99aa8
Pushpak Koli <pushpakkoli895@gmail.com>
2026-06-18 00:20:50 +0530

    Remove username from public_key_path default value

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -18,10 +18,10 @@ variable "public_key_path" {
   type        = string
   description = "Local path to the public SSH key file to upload to AWS key pair (not the private .pem file)."
   # Windows example:
-  # default = "C:\\Users\\pushp\\.ssh\\id_rsa.pub"
+  # default = "C:\\Users\\your-username\\.ssh\\id_rsa.pub"
   # Ubuntu/Linux example:
-  # default = "/home/youruser/.ssh/id_rsa.pub"
-  default     = "C:\\Users\\pushp\\.ssh\\id_rsa.pub"
+  # default = "/home/your-username/.ssh/id_rsa.pub"
+  default     = ""
 }
 
 variable "ami_id" {
@@ -29,7 +29,27 @@ variable "ami_id" {
   description = "Optional EC2 AMI ID to use. Leave blank to use the latest Amazon Linux 2023 AMI."
   default     = "ami-0e38835daf6b8a2b9"
 }
-
+*.pem
+terraform.tfvars
+terraform.tfstate*
+.env
+.DS_Store
+*.tfvars.json*.pem
+terraform.tfvars
+terraform.tfstate*
+.env
+.DS_Store
+*.tfvars.json*.pem
+terraform.tfvars
+terraform.tfstate*
+.env
+.DS_Store
+*.tfvars.json*.pem
+terraform.tfvars
+terraform.tfstate*
+.env
+.DS_Store
+*.tfvars.json
 variable "create_key_pair" {
   type        = bool
   description = "Whether Terraform should import/create the SSH key pair in AWS. Set false to use an existing key pair name."
